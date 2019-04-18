3fc0c9e59dc1875f257cc232a1b9928a3d779b08
jiayi1129 <35002411+jiayi1129@users.noreply.github.com>
2019-04-18 14:20:58 +0800

    Update application.py

--- application.py
+++ application.py
@@ -13,8 +13,6 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Ensure responses aren't cached
-
 
 @app.after_request
 def after_request(response):
@@ -29,11 +27,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# connecting to the database
-connection = sqlite3.connect("users.db")
-
-# cursor
-db = connection.cursor()
+# Remember Session
+session["user_id"]
 
 @app.route("/", methods = ["GET"])
 @login_required
